82b0140a0093b5219af07990fef5947440004d2b
Dmitry M <dmitrym@gmail.com>
2022-01-23 15:34:07 -0800

    Basic structure in place; as well as some tests.

--- org-hyperscheduler.el
+++ org-hyperscheduler.el
@@ -85,20 +85,21 @@ Takes _WS and FRAME as arguments."
   (let* ((props (org-entry-properties))
          (js-date (get-js-date-pair ))
          )
+    (print props)
     (push `(startDate . ,(cdr (assoc 'startDate js-date))) props)
     (push `(endDate . ,(cdr (assoc 'endDate js-date))) props)
+    (push `(allDay . ,(cdr (assoc 'allDay js-date))) props)
     props
     )
   )
 
-(defun get-calendar-entries ()
-  (org-map-entries #'get-agenda "/+TODO" 'agenda)
-  )
+(defun get-calendar-entries (scope)
+    (org-map-entries #'get-agenda "TODO=\"TODO\"+SCHEDULED>=\"<2008-10-11>\"" nil))
           
 (provide 'org-hyperscheduler)
 
 (defun org-hs--get-agenda ()
-    (websocket-send-text org-roam-hs-ws-socket (json-encode (get-dummy-schedule)))
+    (websocket-send-text org-roam-hs-ws-socket (json-encode (get-calendar-entries)))
   )
 
 
@@ -117,23 +118,21 @@ Takes _WS and FRAME as arguments."
          (day-end (plist-get plist :day-end))
          (hour-end (plist-get plist :hour-end))
          (minute-end (plist-get plist :minute-end))
-         (minute-start "00")
-         (minute-end "00")
-         (month-start "01")
-         (month-end "01")
-         (start (format "%s-%s-%sT%s:%s-08:00" year-start month-start day-start hour-start minute-start))
-         (end (format "%s-%s-%sT%s:%s-08:00" year-end month-end day-end hour-end minute-end))
-         (combined `((startDate . ,start) ( endDate . ,end)))
-         )
+         (start (date-time-to-iso8601-js-like 0 minute-start hour-start day-start month-start year-start))
+         (end (date-time-to-iso8601-js-like 0 minute-end hour-end day-end month-end year-end) )
+         (all-day (if (eq hour-start nil) "true" "false"))
+         (combined `((startDate . ,start) ( endDate . ,end) (allDay . ,all-day))))
     combined
     )
   )
 
 (defun date-time-to-iso8601-js-like (seconds minutes hour day month year)
-   (concat
-    (format-time-string "%Y-%m-%dT%T"  (encode-time seconds hour minutes day month year))
-    ((lambda (x) (concat (substring x 0 3) ":" (substring x 3 5)))
-(format-time-string "%z"))))
+  (message (format "params %s %s %s %s %s %s" seconds minutes hour day month year))
+  (let* ((minutes (or minutes 0))
+        (hour (or hour 0)))
+    (concat
+     (format-time-string "%Y-%m-%dT%T"  (encode-time seconds minutes hour day month year))
+     ((lambda (x) (concat (substring x 0 3) ":" (substring x 3 5)))
+      (format-time-string "%z")))))
 
-(date-time-to-iso8601-js-like 0 0 15 21 1 2022)
 
--- tests/test-org-hyperscheduler.el
+++ tests/test-org-hyperscheduler.el
@@ -3,24 +3,52 @@
 
 
 (defvar mock-org-contents
-"* Heading 1
-** Subheading 2
-*** TODO a task aaa
-*** TODO a second task
+"* TODO a task aaa
+SCHEDULED: <2022-01-23 Sun>
+* TODO a second task
+SCHEDULED: <2022-01-23 Sun 14:00-15:00>
 ")
 
-(describe "Setup the contents"
-          (it "can iterate over them"
+(describe "Getting the agenda"
+          (it "can get the correct entries"
               (with-temp-buffer
                 (org-mode)
                 (insert mock-org-contents)
-                (let ((number-of-todo-entries (length (get-calendar-entries))))
+                (let ((number-of-todo-entries (length (get-calendar-entries nil))))
                   (expect number-of-todo-entries :to-be 2))))
 
           (it "can produce a json representation"
                (with-temp-buffer
                 (org-mode)
                 (insert mock-org-contents)
-                (let ((json-representation (json-encode (get-calendar-entries))))
+                (let ((json-representation (json-encode (get-calendar-entries nil))))
                   (expect (string-match "a task aaa" json-representation) :not :to-be nil))))
           )
+
+
+(describe "ISO8601 date formatting"
+          (it "can parse the dates correctly"
+          (with-temp-buffer
+            (insert mock-org-contents)
+            (org-mode)
+            (let ((js-date (get-js-date-pair)))
+              (expect (cdr (assoc 'startDate js-date)) :to-equal "2022-01-23T14:00:00-08:00")
+              (expect (cdr (assoc 'endDate js-date)) :to-equal "2022-01-23T15:00:00-08:00")
+              (expect (cdr (assoc 'allDay js-date)) :to-equal "false")
+              )
+            )
+          )
+
+
+          (it "can detect all day correctly"
+          (with-temp-buffer
+            (insert mock-org-contents)
+            (org-mode)
+            (org-next-visible-heading -1) ;; seems kinda flaky?
+            (beginning-of-buffer)
+            (let ((js-date (get-js-date-pair)))
+              (expect (cdr (assoc 'allDay js-date)) :to-equal "true")
+              )
+            )
+          ))
+
