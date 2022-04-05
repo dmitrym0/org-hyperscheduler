dee1a2a2eca213d91f20a13ece45ecab654d4375
Dmitry M <dmitrym@gmail.com>
2022-04-05 15:56:07 -0700

    Add eslint config.

---
+++ calendar/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "jquery": true,
+
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+    },
+    "globals": {
+        "tui": true
+    }
+
+}
