e5d1daddb7bc96d2f2f256f81de4db76a303ea41
Dmitry M <dmitrym@gmail.com>
2022-07-13 11:43:38 -0400

    Re-render changes from react query as they come in.

--- calendar-react/org-hyperscheduler/src/App.tsx
+++ calendar-react/org-hyperscheduler/src/App.tsx
@@ -52,49 +52,37 @@ persistQueryClient({
 
 
 const bindWebSocket = () => {
-  console.log(" websocket INIT INIT INIT");
+  console.log("[websocket] Initializing..");
   const websocket = new WebSocket("ws://127.0.0.1:44445");
 
   websocket.onopen = () => {
-    console.log('connected')
+    console.log('[websocket] Connected')
 
   }
 
 
   websocket.onmessage = (event) => {
-    console.log("ON MESSAGE");
-    // const data = JSON.parse(event.data)
-    // const queryKey = [...data.entity, data.id].filter(Boolean)
-    // queryClient.invalidateQueries(queryKey)
-
+    console.log("[websocket] Global websocket handler");
   }
 
   return websocket;
 
 }
 
-
 const websocket = bindWebSocket()
 
-
-// TODO:
-// 1. on fetch agenda, de-register normal handler..
-// 2. register an agenda specific handler
-// 3. This agenda specific handler returns to useQuery??
-// 4. Reregister the standard handler
-//
-// error checking in handlers?
 const fetchAgenda = () => {
   return syncSend({ "command": "get-agenda" });
 }
 
 // a helper that turns websocket messaging into a synchronous affair.
 const syncSend = (payload) => {
+  console.log(`[websocket] syncSend ${JSON.stringify(payload)}`);
   const string_payload = JSON.stringify(payload);
   websocket.send(string_payload);
   var promise = new Promise(function(resolve, reject) {
     let syncResponseHandler = (event) => {
-      console.log(`Response for ${string_payload}`);
+      console.log(`[websocket] Response for ${string_payload}`);
       const data = JSON.parse(event.data);
       resolve(data);
       websocket.removeEventListener('message', syncResponseHandler);
@@ -104,14 +92,27 @@ const syncSend = (payload) => {
   return promise;
 }
 
+export const useAgenda = () => useQuery(["agenda"],
+  fetchAgenda,
+  {
+    //staleTime: 30 * 1000,
+    // placeholderData: () => {
+    //   console.log("[rQuery] placeholder data");
+    // },
+    // initialData: () => {
+    //   console.log("[rQuery] checking initial data");
+    //   // Get the query state
+    //   const state = queryClient.getQueryState(['agenda'])
 
+    //   // // If the query exists and has data that is no older than 10 seconds...
+    //   // if (state && Date.now() - state.dataUpdatedAt <= 10 * 1000) {
+    //   //   // return the individual todo
+    //   //   return state.data.find(d => d.id === todoId)
+    //   //}
 
-
-
-export const useAgenda = () => useQuery(["agenda"], fetchAgenda);
-
-
-
+      // Otherwise, return undefined and let it fetch from a hard loading state!
+    //},
+  });
 
 const calendars = [
     {
@@ -135,6 +136,11 @@ const calendars = [
 
 const parseAgenda = (agenda) => {
   const schedule = [];
+  if (agenda === undefined) {
+    console.warn("Empty agenda!");
+    return;
+  }
+
   for (const agendaItem of agenda) {
     let calendarItem = {
       id: agendaItem["ID"],
@@ -166,16 +172,28 @@ const parseAgenda = (agenda) => {
 
 
 export function MyCalendar(props) {
+  const containerRef = useRef(); // TODO: Magic, how does this work?
+  const calendarRef = useRef(); // TODO: Magic, how does this work?
+
+
+  const updateSchedule = (agenda) => {
+    if (agenda !== undefined) {
+      calendarRef.current?.clear();
+      calendarRef.current?.createSchedules(agenda);
+    }
+  }
+
+
+
   const { isLoading, error, data } = useAgenda();
 
   if ( isLoading ) {
     return <div> Loading </div>
+  } else {
+    updateSchedule(parseAgenda(data["agenda"]));
   }
 
-  const containerRef = useRef(); // TODO: Magic, how does this work?
-  const calendarRef = useRef(); // TODO: Magic, how does this work?
-
-  console.log("Rendering MyCalendar " + calendarRef.current);
+ console.log("- Rendering MyCalendar " + calendarRef.current);
 
   useEffect(() => {
     console.log("Creating calendar instance.");
@@ -202,7 +220,7 @@ export function MyCalendar(props) {
 
 
     if (calendarRef.current) {
-      calendarRef.current?.createSchedules(parseAgenda(data));
+      updateSchedule(parseAgenda(data["agenda"]));
     }
 
     return () => {
