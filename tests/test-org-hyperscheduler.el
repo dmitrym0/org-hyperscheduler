a0828c4eb4507019c69321e1c1e822e32f711a2c
Dmitry M <dmitrym@gmail.com>
2022-07-18 20:43:44 -0700

    Set time zone for every test.
    Not sure why it's failing on CI.

--- tests/test-org-hyperscheduler.el
+++ tests/test-org-hyperscheduler.el
@@ -28,9 +28,6 @@ SCHEDULED: <2022-01-23 Sun>
 (org-hs--log-set-level 'debug)
 
 
-;; set time zone. all expectations are for PST time.
-(set-time-zone-rule "US/Pacific")
-
 ;; this is how I ship org-hs: readonly mode enabled, org-roam exclusion disabled.
 (defun org-hs-default-state ()
   (setq org-hyperscheduler-exclude-from-org-roam nil)
@@ -49,6 +46,10 @@ SCHEDULED: <2022-01-23 Sun>
 ;; TODO: asserts can fail and that kills the stack, catch the exception so that we can do proper cleanup.
 (defun with-mock-contents (contents lambda)
   (org-mode)
+  ;; set time zone. all expectations are for PST time.
+  (set-time-zone-rule "US/Pacific")
+
+
   (setq org-id-track-globally t)
 
 
